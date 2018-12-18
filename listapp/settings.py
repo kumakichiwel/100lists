191c7cd544f8b955fd0ae88338b6d751257e716d
kumakichiwel <koji.ito@sekai-inc.jp>
2018-12-18 18:42:03 +0900

    settings.py test

--- listapp/settings.py
+++ listapp/settings.py
@@ -14,8 +14,8 @@ import os
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -134,6 +134,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     os.path.join(BASE_DIR, 'static/css'),
     os.path.join(BASE_DIR, 'static/fonts'),
+    os.path.join(BASE_DIR, 'static/js'),
 ]
 
 
