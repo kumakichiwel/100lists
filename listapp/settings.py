184b231d282204bdba5db0afa88f226cebad2e88
kumakichiwel <koji.ito@sekai-inc.jp>
2018-12-18 18:32:37 +0900

    update settings.py

--- listapp/settings.py
+++ listapp/settings.py
@@ -132,6 +132,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static/css'),
+    os.path.join(BASE_DIR, 'static/fonts'),
 ]
 
 
