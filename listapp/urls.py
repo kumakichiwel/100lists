5f98fcd19fa196fa393efa1460bcc28d3313bc72
kumakichiwel <koji.ito@sekai-inc.jp>
2018-12-19 09:48:03 +0900

    login test

--- listapp/urls.py
+++ listapp/urls.py
@@ -20,9 +20,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('list.urls')),
-    path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
