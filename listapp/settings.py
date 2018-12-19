b8c9b24901584c9d15d447720d6a1b5840410545
kumakichiwel <koji.ito@sekai-inc.jp>
2018-12-19 10:12:39 +0900

    login test

--- listapp/settings.py
+++ listapp/settings.py
@@ -41,12 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'list.apps.ListConfig',
-    'accounts.apps.AccountsConfig',
     'widget_tweaks',
     'bootstrap_datepicker_plus',
     'imagekit',
     'django_cleanup',
+    'list.apps.ListConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 
@@ -117,7 +117,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'ja'
+LANGUAGE_CODE = 'ja-JP'
 
 TIME_ZONE = 'Asia/Tokyo'
 
@@ -130,12 +130,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'static/css'),
-]
+)
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
