5169d746fecbfca26b6b3571f46ae5ff53bf0764
kumakichiwel <koji.ito@sekai-inc.jp>
2018-12-21 13:00:15 +0900

    AWS update

--- listapp/settings.py
+++ listapp/settings.py
@@ -167,10 +167,11 @@ if not DEBUG:
     AWS_S3_OBJECT_PARAMETERS = {
         'CacheControl': 'max-age=86400',  # 1日はそのキャッシュを使う
     }
-    
+    '''    
     AWS_LOCATION = 'static'
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-
+    '''
+    
     DEFAULT_FILE_STORAGE = 'listapp.storage_backends.MediaStorage'
 
